f9c7754de4e1292eadf0cef17da09628486bad66
Tom Ezron <tomezron@gmail.com>
2016-03-31 12:43:21 +0300

    Add a test ground project

---
+++ TestGround/Source/TestGround/TestGroundGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TestGroundGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTGROUND_API ATestGroundGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
